e250bf4b5b1ceb7677bb4cf876bedb0a021a9f23
baheldeepti <137839695+baheldeepti@users.noreply.github.com>
2025-04-13 03:45:43 -0700

    Create hospitalapp_wip.py

---
+++ hospitalapp_wip.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Hospital Insights", layout="wide")
+st.title("🏥 Welcome to the Hospital Analytics Assistant")
+st.markdown("Navigate using the sidebar to explore dashboard metrics or chat with the AI assistant.")
